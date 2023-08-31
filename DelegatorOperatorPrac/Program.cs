af30d99741ffd590128a446a5b5be7f24a5ba3de
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-08-31 15:09:46 +0900

    델리게이트 연산

--- DelegatorOperatorPrac/Program.cs
+++ DelegatorOperatorPrac/Program.cs
@@ -8,8 +8,26 @@ namespace DelegatorOperatorPrac
 {
     class Program
     {
+        public delegate void SendString(string message);
         static void Main(string[] args)
         {
+            SendString sayHello, sayGoodbye, multiDelegate;
+            sayHello = Hello;
+            sayGoodbye = GoodBye;
+            multiDelegate = sayHello + sayGoodbye;
+            multiDelegate("김윤서");
+            multiDelegate -= sayGoodbye;
+            multiDelegate("김윤서");
+        }
+
+        private static void GoodBye(string message)
+        {
+            Console.WriteLine("안녕히가세요, " + message + "양..!");
+        }
+
+        private static void Hello(string message)
+        {
+            Console.WriteLine("안녕하세요, " + message + "양..!");
         }
     }
 }
